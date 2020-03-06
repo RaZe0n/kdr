0991e4fa3554149b7f79f01f1004efc275e58f31
RaZe0n <finn.bruinzeel@gmail.com>
2020-03-06 16:48:12 +0100

    review 1

--- commands/review.js
+++ commands/review.js
@@ -4,29 +4,29 @@ module.exports.run = async(bot, message, args) =>{
 
   const totalStars = args[0];
 
-if(!totalStars || totalStars < 1 || totalStars > 5) return message.channel.send("Please give your stars between 1 and 5.");
+  if(!totalStars || totalStars < 1 || totalStars > 5) return message.channel.send("Please give your stars between 1 and 5.");
 
-const reviewMessage = args.splice(1, args.length).join(' ') || '**Please send a message**';
+  const reviewMessage = args.splice(1, args.length).join(' ') || '**Please send a message**';
 
-var reviewChannel = message.guild.channels.find("name", "review");
-if(!reviewChannel) return console.log("review> Channel not found!");
+  var reviewChannel = message.guild.channels.find("name", "review");
+  if(!reviewChannel) return console.log("review> Channel not found!");
 
-var stars = "";
+  var stars = "";
 
-for(var i =0; i < totalStars; i++) {
-  stars += ":star: ";
-}
+  for(var i =0; i < totalStars; i++) {
+    stars += ":star: ";
+  }
 
-message.delete();
+  message.delete();
 
-const reviewEmbed = new discord.RichEmbed()
-.setTitle(`${message.author.username} has made a review! :tada:`)
-.setColor("$00ff00")
-.addField("Stars: ", `${stars}`)
-.addField("Review: ", `${reviewMessage}`);
+  const reviewEmbed = new discord.RichEmbed()
+  .setTitle(`${message.author.username} has made a review! :tada:`)
+  .setColor("$00ff00")
+  .addField("Stars: ", `${stars}`)
+  .addField("Review: ", `${reviewMessage}`);
 
-message.channel.send(":white_check_mark: You have succesfully made a review!");
-return reviewChannel.send(reviewEmbed);
+  message.channel.send(":white_check_mark: You have succesfully made a review!");
+  return reviewChannel.send(reviewEmbed);
 
 }
 
